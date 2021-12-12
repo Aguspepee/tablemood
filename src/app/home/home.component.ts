7926f349dd1ed6ccc733acf14a23ecb3f3b23bf2
Aguspepee <sanchezagustinezequiel@gmail.com>
2021-12-12 20:47:37 -0300

    Main

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  nombre="pepe";
+  
+  constructor() {
+   }
 
-  constructor() { }
+   cambiarTitulo(){
+    this.nombre="jose"
+
+   }
 
   ngOnInit(): void {
   }
